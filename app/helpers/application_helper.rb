d7f17669248070f791c1aed886c350772b1ddcd8
EC2 Default User <ec2-user@ip-172-31-18-67.us-east-2.compute.internal>
2019-11-06 15:04:11 +0000

    Task content hyperlink

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
       page_name + " | " + base_title
     end
   end
+
+  require "uri"
+
+  def text_url_to_link(text)
+
+    URI.extract(text, ["http", "https"]).uniq.each do |url|
+      text.gsub!(url, "<a href=\"#{url}\" target=\"_blank\">#{url}</a>")
+    end
+    text
+  end
+  
 end
